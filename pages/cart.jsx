4604503249c31ecc568592e606b02c58bb0f4319
basem-asem <120086391+basem-asem@users.noreply.github.com>
2023-07-14 16:34:00 +0300

    change useEffect dep

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -48,7 +48,7 @@ const Cart = () => {
           currency: currency,
         },
       });
-    }, [currency, showSpinner, dispatch, options]);
+    }, [currency, showSpinner,]);
 
     return (
       <>
