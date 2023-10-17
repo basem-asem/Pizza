c1abdd7b8fdcb6c85e775c6d1dc39f06987cb5bb
Basem Asem <120086391+basem-asem@users.noreply.github.com>
2023-10-18 01:28:22 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,24 @@
+import React, { useState, useEffect } from "react";
 import Featured from "@/Components/Featured";
 import PizzaList from "@/Components/PizzaList";
 import AddButton from "@/Components/AddButton";
 import Head from "next/head";
 import axios from "axios";
-import { useState, useEffect } from "react";
-import Add from "@/Components/Add";
 
 export default function Home({ pizzaList, admin }) {
   const [loading, setLoading] = useState(true);
-  const [showAdd, setShowAdd] = useState(false); // Rename 'close' to 'showAdd'
+  const [showAdd, setShowAdd] = useState(false);
+
+  // Watch for changes in pizzaList props
+  useEffect(() => {
+    if (pizzaList.length > 0) {
+      console.log("pizza is here");
+      // Handle logic when pizzaList is not empty
+    }
+  }, [pizzaList]); // Re-run the effect whenever pizzaList changes
 
   useEffect(() => {
-    setLoading(false)
+    setLoading(false);
   }, []);
 
   return (
@@ -21,17 +28,16 @@ export default function Home({ pizzaList, admin }) {
         <meta name="description" content="Best pizza shop in town" />
       </Head>
       <Featured />
-      {admin && <AddButton setClose={() => setShowAdd(true)} />} {/* Update setClose */}
+      {admin && <AddButton setClose={() => setShowAdd(true)} />}
       {loading ? (
         <p>Loading...</p>
       ) : (
         <PizzaList pizzaList={pizzaList} />
       )}
-      {showAdd && <Add setClose={() => setShowAdd(false)} />} {/* Update showAdd */}
+      {showAdd && <Add setClose={() => setShowAdd(false)} />}
     </div>
   );
 }
-
 
 export async function getServerSideProps(ctx) {
   const myCookie = ctx.req?.cookies || {};
@@ -52,7 +58,7 @@ export async function getServerSideProps(ctx) {
   } catch (error) {
     console.error("Error fetching data:", error.message);
     return {
-      props: {       
+      props: {
         pizzaList: [],
         admin,
       },
